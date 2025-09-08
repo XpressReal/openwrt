33a3a0c0b7dfac2797a1dce6b034d502d98d6a83
fyde <fyde@fydeos.io>
2025-09-08 18:18:54 +0800

    genimage

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -2042,3 +2042,33 @@ define KernelPackage/enc28j60/description
 endef
 
 $(eval $(call KernelPackage,enc28j60))
+
+define KernelPackage/aic8800-wlan
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=AIC8800 SDIO wireless module
+  KCONFIG:=CONFIG_AIC8800_WLAN_SUPPORT
+  FILES:= \
+         $(LINUX_DIR)/bsp/drivers/net/wireless/aic8800/aic8800_fdrv/aic8800_fdrv.ko \
+         $(LINUX_DIR)/bsp/drivers/net/wireless/aic8800/aic8800_bsp/aic8800_bsp.ko
+  AUTOLOAD:=$(call AutoProbe,aic8800_fdrv aic8800_bsp)
+endef
+
+define KernelPackage/aic8800-wlan/description
+  Kernel module for AIC8800 SDIO wireless module
+endef
+
+$(eval $(call KernelPackage,aic8800-wlan))
+
+define KernelPackage/aic8800-bt
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=AIC8800 SDIO bluetooth module
+  KCONFIG:=CONFIG_AIC8800_BTLPM_SUPPORT
+  FILES:=$(LINUX_DIR)/bsp/drivers/net/wireless/aic8800/aic8800_btlpm/aic8800_btlpm.ko
+  AUTOLOAD:=$(call AutoProbe,aic8800_btlpm)
+endef
+
+define KernelPackage/aic8800-bt/description
+  Kernel module for AIC8800 SDIO bluetooth module
+endef
+
+$(eval $(call KernelPackage,aic8800-bt))
--- target/linux/rtd/image/rtd1619b.mk
+++ target/linux/rtd/image/rtd1619b.mk
@@ -1,7 +1,7 @@
-define Device/xpressreal-t3
+define Device/rtd_xpressreal-t3
   DEVICE_VENDOR := XpressReal
   DEVICE_MODEL := T3
   SOC := rtd1619b
   DEVICE_DTS := realtek/rtd1619b-bleedingedge-4gb
 endef
-TARGET_DEVICES += xpressreal-t3
+TARGET_DEVICES += rtd_xpressreal-t3
