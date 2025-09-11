544fa0d02748887e2c253a27a8891af0d3d39d28
fyde <fyde@fydeos.io>
2025-09-11 17:13:42 +0800

    move firmwares to packages

--- target/linux/rtd/image/rtd1619b.mk
+++ target/linux/rtd/image/rtd1619b.mk
@@ -3,6 +3,6 @@ define Device/rtd_xpressreal-t3
   DEVICE_MODEL := T3
   SOC := rtd1619b
   DEVICE_DTS := realtek/rtd1619b-bleedingedge-4gb
-  DEVICE_PACKAGES := kmod-aic8800-bt kmod-aic8800-wlan luci
+  DEVICE_PACKAGES := aic8800-firmware realtek-rtd1619b-firmware kmod-aic8800-bt kmod-aic8800-wlan
 endef
 TARGET_DEVICES += rtd_xpressreal-t3
